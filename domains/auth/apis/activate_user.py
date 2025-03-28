f6c11730277a1ed0d9f29e833507410a81d7b2d3
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2025-03-28 03:22:41 +0000

    add missing scripts

---
+++ domains/auth/apis/activate_user.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, status, Depends
+from pydantic import UUID4
+from sqlalchemy.orm import Session
+
+from crud.base_schema import HTTPError
+from db.session import get_db
+from domains.auth.models import User
+from domains.auth.oauth import get_current_user
+from domains.auth.services.user import user_service
+
+activate_user_router = APIRouter(prefix="/user")
+
+
+@activate_user_router.post(
+    "/{id}/activate",
+    name="activate_account",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={status.HTTP_404_NOT_FOUND: {"model": HTTPError}},
+)
+async def activate_user(
+        *, db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_user),
+        id: UUID4
+) -> None:
+    await user_service.activate_user(db=db, id=id)
---
+++ domains/auth/apis/get_current_user.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from db.session import get_db
+from domains.auth.models import User
+from domains.auth.oauth import get_current_user
+from domains.auth.schemas.user import UserSchema
+from domains.auth.services.user import user_service
+
+current_user_router = APIRouter(prefix="/users")
+
+
+@current_user_router.get("/me", response_model=UserSchema)
+async def get_current_user_details(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await user_service.get_user(db=db, id=current_user.id)
